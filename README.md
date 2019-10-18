02567be1b0d898d77e49182f2d092554caceb2b8
Jenwng <Jenniescwong@gmail.com>
2019-10-17 22:44:45 -0400

    CSS for mobile app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # snake-ladder
+
 Snake and ladder math addition game
+
+The goal of the game is for the player to reach the finish line, square #100.
+
+The player has to answer math questions. The player will start from square #0 and answers what position he or she lands on based on the rolls of the two dices.
+
+The dices give out random numbers. The resut of the two dices will be the number of squares that the player advances to, if his/her answer is correct. If the answer is incorrect, the player will move back by the numbers from the dices.
+
+The player repeat the process above until he/she reacheas the finish line, or the player has the option to reset the game.
+
+API used:
+
+Giphy API Key: 8VJTRcjSYCnl2h7MQxOwwSCqBnp7LeJi
--- app.js
+++ app.js
@@ -4,20 +4,58 @@ $(() => {
   // variables here
   const endpoint = "https://api.thecatapi.com/v1/images/search?";
 
+  const endpoint2 =
+    "https://api.giphy.com/v1/stickers/search?api_key=8VJTRcjSYCnl2h7MQxOwwSCqBnp7LeJi&q=winner cat &limit=25&offset=0&rating=G&lang=en";
+
+  const endpoint3 =
+    "https://api.giphy.com/v1/stickers/search?api_key=8VJTRcjSYCnl2h7MQxOwwSCqBnp7LeJi&q=snake&limit=25&offset=0&rating=G&lang=en";
+
+  const endpoint4 =
+    "https://api.giphy.com/v1/gifs/search?api_key=8VJTRcjSYCnl2h7MQxOwwSCqBnp7LeJi&q=cat climbing ladder&limit=25&offset=0&rating=G&lang=en";
+
   let diceNum1 = 0;
   let diceNum2 = 0;
   let playerName = "";
   let currentPosition = 0;
   let catImg = "";
+  let winnerImg = "";
+  let snakeImg = "";
 
   //  callback functions here:
 
-  //  get cat picture function
+  //  get cat picture for player function
   const pullCat = table => {
     catImg = $("<img>")
       .addClass("catpix")
       .attr("src", table[0].url);
     $("body").append(catImg);
+  };
+
+  //   get winner stickery on click of trophy
+  const pullSticker = data => {
+    console.log(data);
+    console.log(data.data[0].url);
+    winnerImg = $("<img>")
+      .addClass("winner")
+      .attr("src", data.data[0].images.original.url);
+    $("#main-grid").append(winnerImg);
+    $("#trophy").hide();
+  };
+
+  // get snake sticker
+  const pullSnake = image => {
+    snakeImg = $("<img>")
+      .addClass("snake")
+      .attr("src", image.data[0].images.original.url);
+    $(`#${currentPosition}`).append(snakeImg);
+  };
+
+  //   get snake climbing ladder giphy
+  const pullLadder = giphy => {
+    ladderImg = $("<img>")
+      .addClass("ladder")
+      .attr("src", giphy.data[0].images.original.url);
+    $(`#${currentPosition}`).append(ladderImg);
   };
 
   // generate random numbers from 1 to 6 inclusive for dice 1
@@ -55,7 +93,7 @@ $(() => {
     $("#myModal").css("display", "flex");
 
     $("#player-name").text(
-      `${playerName}, please enter, in the box below, the square number you should move forward to.`
+      `${playerName}, please enter the square number you should move forward to.`
     );
   };
 
@@ -71,7 +109,8 @@ $(() => {
     playerName = "";
     currentPosition = 0;
     catImg = "";
-    $("100").detach(catImg);
+    $.ajax({ url: endpoint }).then(pullCat);
+    // $("100").detach(catImg);
   };
 
   const convert = () => {
@@ -108,6 +147,7 @@ $(() => {
       $("#player-name").text(
         `Correct! ${playerName} can move forward to box #${currentPosition}`
       );
+      $.ajax({ url: endpoint4 }).then(pullLadder);
       $(`#${currentPosition}`).append(catImg);
     } else if (playerAnswer === correctAnswer && correctAnswer === 100) {
       currentPosition = Math.min(correctAnswer, 100);
@@ -117,21 +157,34 @@ $(() => {
       $("#player-name").text(
         `Congratulations ${playerName}!! You reached the finish line! Go ahead and click on the trophy to reveal your prize!`
       );
+      $.ajax({ url: endpoint4 }).then(pullLadder);
       $(`#${currentPosition}`).append(catImg);
+
+      $("#trophy").on("click", event => {
+        catImg.detach();
+        $.ajax({ url: endpoint2 }).then(data => {
+          console.log(data);
+          pullSticker(data);
+        });
+      });
+      // resetGame();
+      // $.ajax({ url: endpoint }).then(pullCat);
     } else if (playerAnswer !== correctAnswer && currentPosition > change) {
-      currentPosition -= change;
       //   insert modal
       $("#myModal").css("display", "flex");
       $("#player-name").text(
         `Sorry ${playerName}, the answer is incorrect, the snake got you and you move back to box #${currentPosition}`
       );
+      $.ajax({ url: endpoint3 }).then(pullSnake);
+      currentPosition -= change;
       $(`#${currentPosition}`).append(catImg);
     } else if (playerAnswer !== correctAnswer && currentPosition < change) {
-      currentPosition = 1;
       $("#myModal").css("display", "flex");
       $("#player-name").text(
         `Sorry ${playerName}, the answer is incorrect, the snake got you and you move back to box #${currentPosition}`
       );
+      $.ajax({ url: endpoint3 }).then(pullSnake);
+      currentPosition = 1;
       $(`#${currentPosition}`).append(catImg);
     }
   };
@@ -139,6 +192,7 @@ $(() => {
   //   event handlers here
   $.ajax({ url: endpoint }).then(pullCat);
 
+  // Game starts below:
   $("#button1").on("click", event => {
     event.preventDefault();
     playerName = $("#input-box1").val();
@@ -158,9 +212,10 @@ $(() => {
     }
   });
 
-  // Game starts below:
   $(".close").on("click", event => {
     $("#myModal").css("display", "none");
+    $(".snake").hide();
+    $(".ladder").hide();
   });
 
   $("#dice1").on("click", rollDice1);
@@ -173,13 +228,9 @@ $(() => {
     resetDice();
   });
 
-  $("#trophy").on("click", event => {
-    catImg.detach();
-    resetGame();
-    $.ajax({ url: endpoint }).then(pullCat);
-  });
-
   $("#button3").on("click", event => {
     resetGame();
   });
+
+  //   $.ajax({ url: endpoint3 }).then(pullSnake);
 });
